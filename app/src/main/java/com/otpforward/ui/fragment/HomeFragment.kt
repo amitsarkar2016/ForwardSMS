58d98a9e619b23028bc364a6fdd7e118ce9a2cdf
amit <amitsarkar007318@gmail.com>
2024-06-24 22:29:10 +0530

    Add Rule Dialog

--- app/src/main/java/com/otpforward/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/otpforward/ui/fragment/HomeFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.otpforward.R
+import com.otpforward.databinding.DialogAddRuleBinding
 import com.otpforward.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private var builder: AlertDialog.Builder? = null
+    private var dialog: AlertDialog? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -28,12 +32,28 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     }
 
+    private fun showAddUpdateDialog() {
+        builder = AlertDialog.Builder(requireContext())
+        val binding = DialogAddRuleBinding.inflate(LayoutInflater.from(requireContext()))
+        builder?.setView(binding.root)
+        dialog = builder?.create()
+
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        binding.addUpdate.setOnClickListener {
+            dialog?.dismiss()
+        }
+        dialog?.show()
+    }
+
     private fun handleViewModels() {
 
     }
 
     private fun handleClicks() {
-
+        binding.addRule.setOnClickListener {
+            showAddUpdateDialog()
+        }
     }
 
     private fun handleSetup() {
